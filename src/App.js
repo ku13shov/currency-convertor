db383d701f7ccbd489edacc45950eae6b972326a
ku13shov <kuleshov000@gmail.com>
2023-07-26 13:22:08 +0300

    add fucntionality

--- src/App.js
+++ src/App.js
@@ -1,14 +1,68 @@
-import React from 'react';
+import { useState, useEffect } from 'react';
+import axios from 'axios';
+
 import { Block } from './Block';
 import './index.scss';
 
 function App() {
-  return (
-    <div className="App">
-      <Block value={0} currency="RUB" onChangeCurrency={(cur) => console.log(cur)} />
-      <Block value={0} currency="USD" />
-    </div>
-  );
+    const [fromCurrency, setFromCurrency] = useState('BYN');
+    const [toCurrency, setToCurrency] = useState('USD');
+    const [fromCurrencyValue, setFromCurrencyValue] = useState(1);
+    const [toCurrencyValue, setToCurrencyValue] = useState(0);
+    const [currencyRate, setCurrencyRate] = useState(0);
+
+    const onChangeFromValue = (value) => {
+        const result = Number.isInteger(value * +currencyRate)
+            ? value * +currencyRate
+            : (value * +currencyRate).toFixed(3);
+
+        setFromCurrencyValue(value);
+        setToCurrencyValue(result);
+    };
+
+    const onChangeToValue = (value) => {
+        const result = Number.isInteger(value / +currencyRate)
+            ? value / +currencyRate
+            : (value / +currencyRate).toFixed(3);
+
+        setToCurrencyValue(value);
+        setFromCurrencyValue(result);
+    };
+
+    const getCurrency = async () => {
+        try {
+            const { data } = await axios.get(
+                `https://cdn.jsdelivr.net/gh/fawazahmed0/currency-api@1/latest/currencies/${fromCurrency.toLocaleLowerCase()}/${toCurrency.toLocaleLowerCase()}.json`,
+            );
+            setCurrencyRate(data[`${toCurrency.toLocaleLowerCase()}`]);
+        } catch (error) {
+            console.error(error);
+        }
+    };
+
+    useEffect(() => {
+        getCurrency();
+        onChangeFromValue(fromCurrencyValue);
+    }, [fromCurrency, toCurrency, currencyRate]);
+
+    return (
+        <div className="App">
+            <Block
+                value={fromCurrencyValue}
+                currency={fromCurrency}
+                onChangeCurrency={setFromCurrency}
+                onChangeValue={onChangeFromValue}
+                currencyRate={currencyRate}
+            />
+            <Block
+                value={toCurrencyValue}
+                currency={toCurrency}
+                onChangeCurrency={setToCurrency}
+                onChangeValue={onChangeToValue}
+                currencyRate={currencyRate}
+            />
+        </div>
+    );
 }
 
 export default App;
